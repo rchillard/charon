abd67fee3b1ebc828af352916d0044efbe8d633d
Ryan <rchillard@gmail.com>
2019-04-01 01:52:31 -0500

    Add basic webpage files for HTML, CSS, and JS

---
+++ webpage/scripts.js
@@ -0,0 +1,18 @@
+// UTILITY FUNCTIONS
+
+// SKY LOGIC
+// Establish time of day based off client
+var bg = document.body;
+// var hour = new Date().getHours();
+
+// Draw background sky color based off of time
+// if (hour >= 5 && hour <= 8) {
+//   bg.classList.add("dawn");
+// } else if (hour > 8 && hour < 18) {
+//   bg.classList.add("day");
+// } else if (hour >= 18 && hour <= 21) {
+//   bg.classList.add("dusk");
+// } else {
+//   bg.classList.add("night");
+//   // Draw starfield
+// }
