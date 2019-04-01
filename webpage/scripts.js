5c464b35c8f1ae78ad3e351a338ece62e235df64
Ryan <rchillard@gmail.com>
2019-04-01 02:11:05 -0500

    Add extra elements for different modes, as well as basic JS for toggling

--- webpage/scripts.js
+++ webpage/scripts.js
@@ -1,18 +1,17 @@
 // UTILITY FUNCTIONS
+function toggleMode() {
+  offlineDisplay.classList.toggle("hidden")
+  onlineDisplay.classList.toggle("hidden")
+}
 
-// SKY LOGIC
-// Establish time of day based off client
-var bg = document.body;
-// var hour = new Date().getHours();
+// EVENT FUNCTIONS
+function handleClick(event) {
+  console.log(event)
+  toggleMode()
+}
 
-// Draw background sky color based off of time
-// if (hour >= 5 && hour <= 8) {
-//   bg.classList.add("dawn");
-// } else if (hour > 8 && hour < 18) {
-//   bg.classList.add("day");
-// } else if (hour >= 18 && hour <= 21) {
-//   bg.classList.add("dusk");
-// } else {
-//   bg.classList.add("night");
-//   // Draw starfield
-// }
+// GATHER ELEMENTS
+var offlineDisplay = document.getElementById("offline")
+var onlineDisplay = document.getElementById("online")
+var payButton = document.getElementById("pay")
+payButton.addEventListener("click", handleClick)
