9eca0d42c6569271c993ac906248e05f9cf3eb63
Ryan <rchillard@gmail.com>
2019-04-02 22:22:04 -0500

    Add basic fetch function to call out to Lambda for status

--- webpage/scripts.js
+++ webpage/scripts.js
@@ -14,4 +14,23 @@ function handleClick(event) {
 var offlineDisplay = document.getElementById("offline")
 var onlineDisplay = document.getElementById("online")
 var payButton = document.getElementById("pay")
+var time = document.getElementById("ip");
 payButton.addEventListener("click", handleClick)
+
+// DETERMINE STATUS
+function getStatus() {
+  return fetch(
+    "https://api-endpoint"
+  )
+    .then(function(response) {
+      console.log(response);
+      return response.json();
+    })
+    .then(function(data) {
+      console.log(data);
+      ip.textContent = data;
+      return data;
+    });
+}
+
+var ip = getStatus();
