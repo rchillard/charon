2e805a5eb49a29a16f09c7a5274807726ad8466f
Ryan <rchillard@gmail.com>
2019-04-04 00:02:51 -0500

    Add functional handler for Lambda function

--- function/handler.js
+++ function/handler.js
@@ -1,20 +1,29 @@
-/* This example describes the specified instance. */
-var AWS = require('aws-sdk');
-AWS.config.update({region: 'us-east-2'});
-
+// AWS Lambda function to check instance state and start if stopped
+const AWS = require('aws-sdk');
 const ec2 = new AWS.EC2({ apiVersion: '2016-11-15' });
 
-var params = {
-    InstanceIds: [
-       "i-###############"
-    ]
+exports.handler = (event, context, callback) => {
+    var params = {
+        InstanceIds: [
+           "i-###############"
+        ]
+    };
+       
+    ec2.describeInstances(params, function(err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else {
+            //  console.log(data.Reservations[0].Instances[0].PublicIpAddress);
+            console.log("Success", JSON.stringify(data));
+            const instanceState = data.Reservations[0].Instances[0].State["Name"]
+            if (instanceState === "stopped") {
+                ec2.startInstances(params, function(err, data) {
+                    if (err) {
+                        console.log("Error", err);
+                    } else if (data) {
+                        console.log("Success", data.StartingInstances)
+                    }
+                })
+            }
+         }
+    });
 };
-
-ec2.describeInstances(params, function(err, data) {
-    if (err) console.log(err, err.stack); // an error occurred
-    else     console.log(data);           // successful response
-     /*
-     data = {
-     }
-     */
-})
